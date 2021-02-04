e53eeb6d445fd8b7abfa6f21889ea5ea1a336439
空明竹随 <26001589+kmzs123@users.noreply.github.com>
2021-02-04 22:13:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+sed -i "/CST-8/a\ \t\tset system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "s/set system.ntp.enable_server='0'/set system.ntp.enable_server='1'/g" package/base-files/files/bin/config_generate
